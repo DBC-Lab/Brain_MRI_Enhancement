68198ce2e89b1a8af1d567529f77b17c6b462bd9
YueSun814 <96925561+YueSun814@users.noreply.github.com>
2024-11-25 13:56:06 -0500

    Add files via upload

--- Reconstruction_test.py
+++ Reconstruction_test.py
@@ -1,12 +1,23 @@
 import SimpleITK as sitk
-
-from multiprocessing import Pool
 import os
-import h5py
 import numpy as np  
-import scipy.io as scio
 from scipy import ndimage as nd
+import argparse
 
+parser = argparse.ArgumentParser(description="Brain MRIs enhancement model")
+
+
+parser.add_argument('--Input', type=str, required=True, help="Path to the input test images, e.g., ./Testing_subjects")
+parser.add_argument('--Output', type=str, required=True, help="Path where the output results will be saved, e.g., ./Testing_subjects")
+parser.add_argument('--Age', type=str, required=True, help="Age group of the test images, i.e., fetal, 0, 3, 6, 9, 12, 18, 24, adult")
+args = parser.parse_args()
+    
+datapath=args.Input #the path to your test images
+age_number=args.Age
+
+if age_number=='adult':
+    age_number='24'
+    
 # Make sure that caffe is on the python path:
 caffe_root = '~/caffe_rc3/'  # Make sure that caffe is on the python path  
 import sys
@@ -21,16 +32,28 @@ solver = None  # ignore this workaround for lmdb data (can't instantiate two sol
 protopath='Pretrained_models/'    #the path to accesss pretrained models and deploy.prototxt
 
 #change pretrained models to match the age of test image, "reconstruction_24m_T1.caffemodel" is used to test images at 24 months and older
-mynet = caffe.Net(protopath+'deploy.prototxt',protopath+'reconstruction_24m_T1.caffemodel',caffe.TEST)     
+if age_number=='fetal':
+    mynet = caffe.Net(protopath+'deploy1.prototxt',protopath+'reconstruction_fetal_T2.caffemodel',caffe.TEST)  
+    print("Model: reconstruction_fetal_T2.caffemodel")
+elif age_number=='24':
+    model_file = 'reconstruction_{}m_T1.caffemodel'.format(age_number)
+    mynet = caffe.Net(protopath + 'deploy.prototxt', protopath + model_file, caffe.TEST) 
+    print("Model: {}".format(model_file))
+else:
+    model_file = 'reconstruction_{}m_T1.caffemodel'.format(age_number)
+    mynet = caffe.Net(protopath + 'deploy1.prototxt', protopath + model_file, caffe.TEST) 
+    print("Model: {}".format(model_file))
+    
+    
 print("blobs {}\nparams {}".format(mynet.blobs.keys(), mynet.params.keys()))
 
 d1=40
 d2=40
 d3=40
-dPatchT1=[d1,d2,d3]  #patch size: 40*40*40
-dPatchT1_ROI=[40,40,40]  #equal or smaller than patch size, to avoid margin issue when testing 
+dFA=[d1,d2,d3]  #patch size: 40*40*40
+dSeg=[40,40,40]  #equal or smaller than patch size, to avoid margin issue when testing 
 
-#step size, usually set as [8, 16]: small step size results in good results but at the cost of long running time.
+#step size, usually set as [8, 16]
 step1=12
 step2=12
 step3=12
@@ -47,69 +70,88 @@ def hist_match(img, temp):
     res = matcher.Execute(img, temp)
     return res
     
-def cropCubic(matT1,fileID,d,step,rate):
+def cropCubic(matFA,fileID,d,step,rate):
     eps=1e-5
     #transpose
-    matT1=np.transpose(matT1,(0,2,1))
+    matFA=np.transpose(matFA,(0,2,1))
 
-    [row,col,leng]=matT1.shape
-    margin1=(dPatchT1[0]-dPatchT1_ROI[0])/2
-    margin2=(dPatchT1[1]-dPatchT1_ROI[1])/2
-    margin3=(dPatchT1[2]-dPatchT1_ROI[2])/2
+    [row,col,leng]=matFA.shape
+    margin1=(dFA[0]-dSeg[0])/2
+    margin2=(dFA[1]-dSeg[1])/2
+    margin3=(dFA[2]-dSeg[2])/2
     cubicCnt=0
     marginD=[margin1,margin2,margin3]
     
-    print 'matT1 shape is ',matT1.shape
-    matT1Out=np.zeros([row+2*marginD[0],col+2*marginD[1],leng+2*marginD[2]])
-    print 'matT1Out shape is ',matT1Out.shape
-    matT1Out[marginD[0]:row+marginD[0],marginD[1]:col+marginD[1],marginD[2]:leng+marginD[2]]=matT1
+    print 'matFA shape is ',matFA.shape
+    matFAOut=np.zeros([row+2*marginD[0],col+2*marginD[1],leng+2*marginD[2]])
+    print 'matFAOut shape is ',matFAOut.shape
+    matFAOut[marginD[0]:row+marginD[0],marginD[1]:col+marginD[1],marginD[2]:leng+marginD[2]]=matFA
 
 
     if margin1!=0:
-        matT1Out[0:marginD[0],marginD[1]:col+marginD[1],marginD[2]:leng+marginD[2]]=matT1[marginD[0]-1::-1,:,:] #reverse 0:marginD[0]
-        matT1Out[row+marginD[0]:matT1Out.shape[0],marginD[1]:col+marginD[1],marginD[2]:leng+marginD[2]]=matT1[matT1.shape[0]-1:row-marginD[0]-1:-1,:,:] #we'd better flip it along the 1st dimension
+        matFAOut[0:marginD[0],marginD[1]:col+marginD[1],marginD[2]:leng+marginD[2]]=matFA[marginD[0]-1::-1,:,:] #reverse 0:marginD[0]
+        matFAOut[row+marginD[0]:matFAOut.shape[0],marginD[1]:col+marginD[1],marginD[2]:leng+marginD[2]]=matFA[matFA.shape[0]-1:row-marginD[0]-1:-1,:,:] #we'd better flip it along the 1st dimension
     if margin2!=0:
-        matT1Out[marginD[0]:row+marginD[0],0:marginD[1],marginD[2]:leng+marginD[2]]=matT1[:,marginD[1]-1::-1,:] #we'd flip it along the 2nd dimension
-        matT1Out[marginD[0]:row+marginD[0],col+marginD[1]:matT1Out.shape[1],marginD[2]:leng+marginD[2]]=matT1[:,matT1.shape[1]-1:col-marginD[1]-1:-1,:] #we'd flip it along the 2nd dimension
+        matFAOut[marginD[0]:row+marginD[0],0:marginD[1],marginD[2]:leng+marginD[2]]=matFA[:,marginD[1]-1::-1,:] #we'd flip it along the 2nd dimension
+        matFAOut[marginD[0]:row+marginD[0],col+marginD[1]:matFAOut.shape[1],marginD[2]:leng+marginD[2]]=matFA[:,matFA.shape[1]-1:col-marginD[1]-1:-1,:] #we'd flip it along the 2nd dimension
     if margin3!=0:
-        matT1Out[marginD[0]:row+marginD[0],marginD[1]:col+marginD[1],0:marginD[2]]=matT1[:,:,marginD[2]-1::-1] #we'd better flip it along the 3rd dimension
-        matT1Out[marginD[0]:row+marginD[0],marginD[1]:col+marginD[1],marginD[2]+leng:matT1Out.shape[2]]=matT1[:,:,matT1.shape[2]-1:leng-marginD[2]-1:-1]
+        matFAOut[marginD[0]:row+marginD[0],marginD[1]:col+marginD[1],0:marginD[2]]=matFA[:,:,marginD[2]-1::-1] #we'd better flip it along the 3rd dimension
+        matFAOut[marginD[0]:row+marginD[0],marginD[1]:col+marginD[1],marginD[2]+leng:matFAOut.shape[2]]=matFA[:,:,matFA.shape[2]-1:leng-marginD[2]-1:-1]
   
-    matT1OutScale = nd.interpolation.zoom(matT1Out, zoom=rate)
+    matFAOutScale = nd.interpolation.zoom(matFAOut, zoom=rate)
 
-    matOut=np.zeros((matT1.shape[0],matT1.shape[1],matT1.shape[2],NumOfClass))
-    Recon=np.zeros((matT1.shape[0],matT1.shape[1],matT1.shape[2]))
+    matOut=np.zeros((matFA.shape[0],matFA.shape[1],matFA.shape[2],NumOfClass))
+    heatmap=np.zeros((matFA.shape[0],matFA.shape[1],matFA.shape[2]))
   
-    Visit=np.zeros((matT1.shape[0],matT1.shape[1],matT1.shape[2]))+eps
-    [row,col,leng]=matT1.shape
+    Visit=np.zeros((matFA.shape[0],matFA.shape[1],matFA.shape[2]))+eps
+    [row,col,leng]=matFA.shape
         
     for i in range(d[0]/2+marginD[0]+1,row-d[0]/2-marginD[0]-2,step[0]):
         for j in range(d[1]/2+marginD[1]+1,col-d[1]/2-marginD[1]-2,step[1]):
             for k in range(d[2]/2+marginD[2]+1,leng-d[2]/2-marginD[2]-2,step[2]):
-                volPatchT1=matT1[i-d[0]/2-marginD[0]:i+d[0]/2+marginD[0],j-d[1]/2-marginD[1]:j+d[1]/2+marginD[1],k-d[2]/2-marginD[2]:k+d[2]/2+marginD[2] ]
+                volFA=matFA[i-d[0]/2-marginD[0]:i+d[0]/2+marginD[0],j-d[1]/2-marginD[1]:j+d[1]/2+marginD[1],k-d[2]/2-marginD[2]:k+d[2]/2+marginD[2] ]
                 
-                if np.sum(volPatchT1)>10 :
+                if np.sum(volFA)>10 :
 
-                    volPatchT1=np.float64(volPatchT1)
+                    volFA=np.float64(volFA)
 
-                    #print 'volPatchT1 shape is ',volPatchT1.shape
-                    mynet.blobs['dataT1'].data[0,0,...]=volPatchT1
+                    #print 'volFA shape is ',volFA.shape
+                    mynet.blobs['dataT1'].data[0,0,...]=volFA
 
                     mynet.forward()
                     temppremat = mynet.blobs['conv6_3-BatchNorm1'].data #Note you have add softmax layer in deploy prototxt
                     Visit[i-d[0]/2:i+d[0]/2,j-d[1]/2:j+d[1]/2,k-d[2]/2:k+d[2]/2]=Visit[i-d[0]/2:i+d[0]/2,j-d[1]/2:j+d[1]/2,k-d[2]/2:k+d[2]/2]+1
-                    Recon[i-d[0]/2:i+d[0]/2,j-d[1]/2:j+d[1]/2,k-d[2]/2:k+d[2]/2]=Recon[i-d[0]/2:i+d[0]/2,j-d[1]/2:j+d[1]/2,k-d[2]/2:k+d[2]/2]+temppremat[0,0,marginD[0]:marginD[0]+d[0],marginD[1]:marginD[1]+d[1],marginD[2]:marginD[2]+d[2]]
+                    heatmap[i-d[0]/2:i+d[0]/2,j-d[1]/2:j+d[1]/2,k-d[2]/2:k+d[2]/2]=heatmap[i-d[0]/2:i+d[0]/2,j-d[1]/2:j+d[1]/2,k-d[2]/2:k+d[2]/2]+temppremat[0,0,marginD[0]:marginD[0]+d[0],marginD[1]:marginD[1]+d[1],marginD[2]:marginD[2]+d[2]]
 
-    Recon = Recon/Visit
-    Recon=np.transpose(Recon,(0,2,1))
+    heatmap = heatmap/Visit
+    heatmap=np.transpose(heatmap,(0,2,1))
     
-    return Recon
+    return heatmap
+ 
+#this function is used to compute the dice ratio
+def dice(im1, im2,tid):
+    im1=im1==tid #make it boolean
+    im2=im2==tid #make it boolean
+    im1=np.asarray(im1).astype(np.bool)
+    im2=np.asarray(im2).astype(np.bool)
+
+    if im1.shape != im2.shape:
+        raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
+    # Compute Dice coefficient
+    intersection = np.logical_and(im1, im2)
+    dsc=2. * intersection.sum() / (im1.sum() + im2.sum())
+    return dsc
 
 def main():
-    datapath='Testing_subjects/' #the path to your test images
-    reference_name = sitk.ReadImage('Templates/Template_T1_24.hdr') #reference file for histogram matching, e.g., Template_T1_24.??? is the reference file for testing images at 24 months and older.
+        
+    if age_number=='fetal':
+        reference_name = sitk.ReadImage('Templates/Template_T2_fetal.nii') 
+        print("Reference image: Template_T2_fetal.nii")
+    else: 
+        reference_name = sitk.ReadImage('Templates/Template_T1_{}.nii'.format(age_number)) #reference file for histogram matching, e.g., Template_T1_24.??? is the reference file for testing images at 24 months and older.
+        print("Reference image: Template_T1_{}.nii".format(age_number))
 
-    files=[i for i in os.listdir(datapath) if '.hdr' in i ]
+    files=[i for i in os.listdir(datapath) if '.nii' in i ]
     for dataT1filename in files:
         myid=dataT1filename[0:len(dataT1filename)-4]
         fileID='%s'%myid
@@ -125,9 +167,18 @@ def main():
         rate=1
         Recon = cropCubic(matched_data_array,fileID,dFA,step,rate)
         
-        volOut=sitk.GetImageFromArray(Recon)
-        volOut.SetSpacing([0.8,0.8,0.8])
-        sitk.WriteImage(volOut,'./{}/{}-recon.nii.gz'.format(datapath, myid))   
+        result_nii=sitk.GetImageFromArray(Recon) 
+        ref_nii = imgOrg
+        result_nii.SetOrigin(ref_nii.GetOrigin())
+    	result_nii.SetDirection(ref_nii.GetDirection())
+    	result_nii.SetSpacing(ref_nii.GetSpacing())
+    	pixelID = result_nii.GetPixelID()
+    	caster = sitk.CastImageFilter()
+    	caster.SetOutputPixelType(pixelID)
+    	result_nii = caster.Execute(result_nii)
+    	result_nii = sitk.Cast(result_nii, sitk.sitkUInt16)  #Uint16 is short type;
+        
+        sitk.WriteImage(result_nii,'./{}/{}-enhanced.nii'.format(datapath, myid))   
 
 
 if __name__ == '__main__':     
